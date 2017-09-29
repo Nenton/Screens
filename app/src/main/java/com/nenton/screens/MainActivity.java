f1c3da92aa8a0529ba1f24772d8f1a52d2b364a9
Nenton <sergejsusev@yandex.ru>
2017-09-29 14:20:32 +0500

    All

--- app/src/main/java/com/nenton/screens/MainActivity.java
+++ app/src/main/java/com/nenton/screens/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
+        setContentView(R.layout.eith_screen);
 
         //Library used
         //https://github.com/AigeStudio/WheelPicker
