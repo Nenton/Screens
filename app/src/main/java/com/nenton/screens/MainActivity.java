d802347b28285ae2801e89f0b020be8147fbb386
Nenton <sergejsusev@yandex.ru>
2017-09-29 10:51:02 +0500

    all refactor picker

--- app/src/main/java/com/nenton/screens/MainActivity.java
+++ app/src/main/java/com/nenton/screens/MainActivity.java
@@ -3,11 +3,24 @@ package com.nenton.screens;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aigestudio.wheelpicker.WheelPicker;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
+        setContentView(R.layout.sixth_screen);
+        WheelPicker wheelPicker = (WheelPicker) findViewById(R.id.data_picker);
+        List<String> strings = new ArrayList<>();
+        strings.add("Mountain View");
+        strings.add("Sunnyvale");
+        strings.add("Cupertino");
+        strings.add("Santa Clara");
+        strings.add("San Jose");
+        wheelPicker.setData(strings);
     }
 }
